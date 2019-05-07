f34f852d5e74a5e501e308f1fbd120ade758c786
Sarah Bardin <srh.fls@gmail.com>
2019-05-06 21:52:56 -0500

    Technical Problem
    Initial Commit

---
+++ technicalProblemBardin/Controllers/TimeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Web.Http;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        // GET api/time
+        [HttpGet]
+        public ActionResult<string> GetTime()
+        {
+            var time = DateTime.Now.ToString();
+            return JsonConvert.SerializeObject(time);
+        }
+    }
+}
